b78d8d496878ab466e4800aba757dcc1714e9787
millalinden <milla.linden@hyperisland.se>
2023-11-27 14:43:15 +0100

    changes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 "use strict";
 
-
 let numSelected = null;
 let tileSelected = null;
-
 let errors = 0;
+let currentLevel = 'easy'; // Added variable to track current level
 
 // Easy level
 let board = [
@@ -17,7 +16,7 @@ let board = [
     "9-4-7---2",
     "67-83----",
     "81--45---"
-]
+];
 // Easy level solution
 let solution = [
     "387491625",
@@ -29,18 +28,45 @@ let solution = [
     "934176852",
     "675832941",
     "812945763"
-]
+];
+
+// Hard level
+let boardHard = [
+    "2-89----7",
+    "--54-7--2",
+    "-9------6",
+    "-----584-",
+    "-3-----6-",
+    "-526-----",
+    "5------7-",
+    "7--5-89--",
+    "6----95-4"
+];
+// Hard level solution
+let solutionHard = [
+    "218936457",
+    "365487192",
+    "497152386",
+    "176295843",
+    "934871265",
+    "852643719",
+    "529314678",
+    "743568921",
+    "681729534"
+];
 
 // When page reload --> the game should be set
-window.onload = function() {
+window.onload = function () {
     setGame();
-}
+};
 
 // How the board will look before playing
 function setGame() {
+    // Clear existing board
+    document.getElementById("board").innerHTML = "";
+
     // Digits 1-9
     for (let i = 1; i <= 9; i++) {
-        // create div id="i"
         let number = document.createElement("div");
         number.id = i;
         number.textContent = i;
@@ -50,13 +76,13 @@ function setGame() {
     }
 
     // Board 9x9
-
+    let currentBoard = (currentLevel === 'easy') ? board : boardHard;
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
-            if (board[r][c] != "-") {
-                tile.innerText = board[r][c];
+            if (currentBoard[r][c] != "-") {
+                tile.innerText = currentBoard[r][c];
                 tile.classList.add("tile-start");
             }
             if (r == 2 || r == 5) {
@@ -70,11 +96,14 @@ function setGame() {
             document.getElementById("board").append(tile);
         }
     }
+
+    // Reset errors count
+    errors = 0;
+    document.getElementById("errors").innerText = errors;
 }
 
 // How to interact with the numbers, adds style when clicked 
-
-function selectNumber(){
+function selectNumber() {
     if (numSelected != null) {
         numSelected.classList.remove("number-selected");
     }
@@ -98,21 +127,26 @@ function selectTile() {
         if (this.innerText != "") {
             return;
         }
-        
-        // "0-0" "0-1"
-        let coords = this.id.split("-"); // ["0", "0"]
+
+        let coords = this.id.split("-");
         let r = parseInt(coords[0]);
         let c = parseInt(coords[1]);
 
-        if (solution[r][c] == numSelected.id) {
+        let currentSolution = (currentLevel === 'easy') ? solution : solutionHard;
+        if (currentSolution[r][c] == numSelected.id) {
             this.innerText = numSelected.id;
-        }
-        else {
+        } else {
             errors++;
             document.getElementById("errors").innerText = errors;
         }
-        
     }
 }
 
+function toggleLevel() {
+    currentLevel = (currentLevel === 'easy') ? 'hard' : 'easy';
+    setGame();
+}
+
+let toggleEl = document.querySelector("#toggle");
+toggleEl.addEventListener("click", toggleLevel);
 
